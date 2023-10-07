b4d6f8d2bc2fb136fc8a077db7fb4930ce8369ee
Tran Thang <tranthang0369@gmail.com>
2023-10-07 12:13:01 +0700

    update

---
+++ js/style.js
@@ -0,0 +1,131 @@
+// $(document).ready(function () {
+//       $name = $('.name').val();
+//       $phone = $('.phone').val();
+//       $address = $('.address').val();
+
+//       if ($name == "")
+//          $('.error').text('Chưa nhập đủ tên');
+
+//       if ($phone == "")
+//          $('.error').text('Chưa nhập đủ số điện thoại');
+
+//       if ($address == "")
+//          $('.error').text('Chưa nhập đủ địa chỉ');
+
+//       if ($name == "" && $phone == "" && $address == "")
+//          $('.error').text('Chưa đủ thông tin');
+//    })
+
+
+
+//    $('.s-slider .slick-slider').slick({
+//       dots: false,
+//       infinite: false,
+//       speed: 300,
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
+//       responsive: [{
+//             breakpoint: 1024,
+//             settings: {
+//                slidesToShow: 3,
+//                slidesToScroll: 3,
+//                infinite: true,
+//                dots: true
+//             }
+//          },
+//          {
+//             breakpoint: 600,
+//             settings: {
+//                slidesToShow: 2,
+//                slidesToScroll: 2
+//             }
+//          },
+//          {
+//             breakpoint: 480,
+//             settings: {
+//                slidesToShow: 1,
+//                slidesToScroll: 1
+//             }
+//          }
+//       ]
+//    });
+// });
+
+   // $('.s-slider-2 .slick-slider').slick({
+   //    dots: false,
+   //    infinite: false,
+   //    speed: 300,
+   //    slidesToShow: 3,
+   //    slidesToScroll: 1,
+   //    responsive: [{
+   //          breakpoint: 1024,
+   //          settings: {
+   //             slidesToShow: 3,
+   //             slidesToScroll: 3,
+   //             infinite: true,
+   //             dots: true
+   //          }
+   //       },
+   //       {
+   //          breakpoint: 600,
+   //          settings: {
+   //             slidesToShow: 2,
+   //             slidesToScroll: 2
+   //          }
+   //       },
+   //       {
+   //          breakpoint: 480,
+   //          settings: {
+   //             slidesToShow: 1,
+   //             slidesToScroll: 1
+   //          }
+   //       }
+   //    ]
+   // });
+
+   // $(document).ready(function () {
+   //    $('.s-slider-2 .slick-slider').slick({
+   //       slidesToShow: 3,
+   //       slidesToScroll: 1,
+   //       responsive: [{
+   //          breakpoint: 992,
+   //          settings: {
+   //             slidesToShow: 2
+   //          }
+   //       }, {
+   //          breakpoint: 400,
+   //          settings: {
+   //             slidesToShow: 1
+   //          }
+   //       }]
+   //    });
+   // });
+
+   // $(document).ready(function () {
+   //    $('.s-slider-3 .slick-slider').slick({
+   //       slidesToShow: 3,
+   //       slidesToScroll: 1,
+   //       responsive: [{
+   //          breakpoint: 992,
+   //          settings: {
+   //             slidesToShow: 2
+   //          }
+   //       }, {
+   //          breakpoint: 400,
+   //          settings: {
+   //             slidesToShow: 1
+   //          }
+   //       }]
+   //    });
+   // });
+//    $(document).ready(function () {
+//       $('.header .s-header .s-seach').click(function () {
+//           $('.header .s-header .s-input').toggleClass('elemen');
+//       })
+//   })
+//   $(document).ready(function () {
+//    $('.header .s-header .s-menu').click(function () {
+//        $('.header .s-header .navation').toggleClass('even');
+//    })
+// })
+
